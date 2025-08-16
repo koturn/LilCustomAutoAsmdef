d9390449cf702a56c9cf020e5c3682842809f0fa
koturn <jeak.koutan.apple@gmail.com>
2025-08-17 03:04:18 +0900

    Initial commit

---
+++ Assets/koturn/LilCustomAutoAsmdef/Editor/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Koturn.LilCustomAutoAsmdef.Editor")]
+[assembly: AssemblyDescription("Auto Assembly Definition File Generator for custom shader of lilToon.")]
+[assembly: AssemblyConfiguration("Release")]
+[assembly: AssemblyCompany("koturn")]
+[assembly: AssemblyProduct("Koturn.LilCustomAutoAsmdef.Editor")]
+[assembly: AssemblyCopyright("Copyright (C) 2025 koturn All Rights Reserverd.")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("61b5bdff-f613-cab4-4b48-888cd88a5449")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyInformationalVersion("1.0.0.0")]
+
+// Metadata
+[assembly: AssemblyMetadata("RepositoryUrl", "https://github.com/koturn/LilCustomAutoAsmdef")]
---
+++ README.md
@@ -0,0 +1,13 @@
+LilCustomAutoAsmdef
+===================
+
+Auto Assembly Definition File Generator for custom shader of lilToon.
+
+## Usage
+
+1. Open the Unity project **without booting in safe mode**.
+2. Import a [unitypackage](https://github.com/koturn/LilCustomAutoAsmdef/releases/latest "koturn/LilCustomAutoAsmdef/releases/latest").
+
+## LICENSE
+
+This software is released under the MIT License, see [LICENSE](LICENSE "LICENSE").
